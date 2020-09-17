15157bd7977039caa1a19e71339f9289c019d01b
Dylan <dylan.vallee@hotmail.com>
2020-09-17 13:39:55 +0200

    Add a second asset for the needle (more large)

--- src/items/needle.py
+++ src/items/needle.py
@@ -3,6 +3,6 @@ from .collectableitem import CollectableItem
 
 class Needle(CollectableItem):
     # The filename of the Needle image.
-    _image_file = 'needle.png'
+    _image_file = 'needle_fat.png'
 
     name = 'Needle'
